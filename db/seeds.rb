6d8503b8f2ffd53adf1f610065eea5ec509d7b4a
Giovanni Malesci <giovanni.malesci@hotmail.com>
2023-11-23 08:00:36 +0100

    Push

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+chinese = { name: 'Yang Pao', address: '7 Round St, Beijing', reviews: 3 }
+italian =  { name: 'Mamma mia', address: '56A Square St, Roma', reviews: 4 }
+japanese = { name: 'Shimutsu', address: '34 Sun St, Tokyo', reviews: 4 }
+french = { name: 'Bagette', address: '3 Moon St, Paris', reviews: 2 }
+belgian = { name: 'Waffle', address: '78 Purple St, Tokyo', reviews: 3 }
+[chinese, italian, japanese, french, belgian].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
